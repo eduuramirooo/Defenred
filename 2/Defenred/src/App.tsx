012f179bbaf1c0dafaa3cedda924b340c23238aa
sandrix23 <159067757+SandraaOrtiz@users.noreply.github.com>
2025-02-04 18:57:56 +0100

    Update App.tsx

--- 2/Defenred/src/App.tsx
+++ 2/Defenred/src/App.tsx
@@ -2,11 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Layout from './Layout'
 import Index from './Index'
 import CasaR from './CasaR'
-<<<<<<< Updated upstream
 import Who from './Who'
-=======
 import Contacto from './Contacto'
->>>>>>> Stashed changes
 import './components/css/App.css'
 
 function App() {
@@ -18,12 +15,9 @@ function App() {
           <Route path='/' element={<Layout />}>
             <Route path='/' element={<Index/>}/>
              <Route path='/casaR' element={<CasaR/>}/> 
-<<<<<<< Updated upstream
              <Route path='who' element={<Who/>}></Route>
              <Route path='*' element={<h1>Not Found</h1>}/>
-=======
-             <Route path='/Contacto' element={<Contacto/>}/> 
->>>>>>> Stashed changes
+             <Route path='contacto' element={<Contacto/>}></Route>
           </Route>
         </Routes>
       </Router>
