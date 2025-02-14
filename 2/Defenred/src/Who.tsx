2f8792503ee6c9e3d360396e8711abd9ea2afc89
eduuramirooo <88739436+eduuramirooo@users.noreply.github.com>
2025-02-14 09:41:29 +0100

    asdadad

--- 2/Defenred/src/Who.tsx
+++ 2/Defenred/src/Who.tsx
@@ -1,13 +1,24 @@
 import { Helmet } from 'react-helmet';
+import { Link } from 'react-router-dom';
 function Who() {
     return (
         <>
             <Helmet>
                 <title>Quiénes Somos - Protección y Tu Red de Derechos</title>
                 <meta name="description" content="Conoce a DEFENRED, tu ONG dedicada a la defensa y 
-                unir los derechos humanos. Descúbrenos para proteger a las personas forzadas a huir." />              
+                unir los derechos humanos. Descúbrenos para proteger a las personas forzadas a huir." />
             </Helmet>
             <div className="who">
+                <div className='banner'>
+                    <div className='texto'>
+                        <h1 >Conocenos más en este artículo de El País</h1>
+                        <div className='grid-50'>
+                            <Link to="https://elpais.com/america-futura/2022-12-13/un-oasis-en-madrid-para-que-los-defensores-de-derechos-humanos-recuperen-el-aliento.html">Miralo</Link>
+                            <a href="#bloquebajo">Miralo</a>
+                        </div>
+                    </div>
+                </div>
+                <div id='bloquebajo'>
                 <h3>
                     Conoce a DEFENRED, tu ONG dedicada a la defensa y unir los
                     derechos humanos.Descúbrenos para proteger a las personas
@@ -24,7 +35,7 @@ function Who() {
                     se benefician de su labor. Aspiramos a un lugar donde los derechos
                     humanos sean respetados por y para todos.
                 </p>
-                    
+
 
                 <p>
                     Historia y Proyectos desde Defenred, nos hemos estado dedicando a la
@@ -36,14 +47,15 @@ function Who() {
                     derechos humanos. Trabajamos incansablemente para proporcionar
                     apoyo integral a quienes lo necesitan.
                 </p>
-            <div className='grid3'>
-            <img src="/img/who3.jpg" alt="" />
-            <img src="/img/who1.jpg" alt="" />
-            <img src="/img/who.jpg" alt="" />
-            <h2>Carmen Peralta</h2>
-            <h2>Loreto Rodríguez de Rivera</h2>
-            <h2>Víctor Arias</h2>
-            </div>
+                <div className='grid3'>
+                    <img src="/img/who3.jpg" alt="" />
+                    <img src="/img/who1.jpg" alt="" />
+                    <img src="/img/who.jpg" alt="" />
+                    <h2>Carmen Peralta</h2>
+                    <h2>Loreto Rodríguez de Rivera</h2>
+                    <h2>Víctor Arias</h2>
+                </div>
+                </div>
             </div>
         </>
     )
